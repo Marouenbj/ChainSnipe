2a2e517f680f81077dac03b72e93ff1bc760a96c
Marouenbj <marwen.mbj@gmail.com>
2024-05-23 12:48:20 +0200

    Update server.js

--- server.js
+++ server.js
@@ -33,21 +33,32 @@ app.use(passport.session());
 // Passport Local Strategy
 passport.use(new LocalStrategy(async (username, password, done) => {
   try {
+    console.log(`Authenticating user: ${username}`);
     const user = await User.findOne({ username });
-    if (!user) return done(null, false, { message: 'Incorrect username.' });
-    if (user.password !== password) return done(null, false, { message: 'Incorrect password.' });
+    if (!user) {
+      console.log('User not found');
+      return done(null, false, { message: 'Incorrect username.' });
+    }
+    if (user.password !== password) {
+      console.log('Incorrect password');
+      return done(null, false, { message: 'Incorrect password.' });
+    }
+    console.log('User authenticated successfully');
     return done(null, user);
   } catch (err) {
+    console.log(`Authentication error: ${err}`);
     return done(err);
   }
 }));
 
 passport.serializeUser((user, done) => {
+  console.log(`Serializing user: ${user._id}`);
   done(null, user._id);
 });
 
 passport.deserializeUser(async (id, done) => {
   try {
+    console.log(`Deserializing user: ${id}`);
     const user = await User.findById(id);
     done(null, user);
   } catch (err) {
