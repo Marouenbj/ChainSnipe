aa49ea597e7eec80763cf4f7d01c343e5834a9d0
Marouenbj <marwen.mbj@gmail.com>
2024-05-24 09:41:56 +0200

    Update database.js

--- config/database.js
+++ config/database.js
@@ -3,12 +3,13 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     const mongoURI = 'mongodb+srv://admin:0zeJULpHFMKmkmQ2@chainsnipe.xp3wetj.mongodb.net/test?retryWrites=true&w=majority&appName=ChainSnipe';
-    await mongoose.connect(mongoURI, {});
-    console.log('MongoDB connected');
-  } catch (err) {
+    mongoose.connect(mongoURI)
+        .then(() => console.log('MongoDB connected'))
+       
+    }
+  catch (err) {
     console.error('MongoDB connection error:', err);
-    process.exit(1);
-  }
+}
 };
 
 module.exports = connectDB;
