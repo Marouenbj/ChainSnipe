292a52d57356788ee8e9b1ee98ab843a4c0490ba
Marouenbj <marwen.mbj@gmail.com>
2024-05-24 11:25:24 +0200

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 });
 
 // Serve static files from the "public" directory
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public'))); // Ensure this line is correct
 
 // Redirect root to login page
 app.get('/', (req, res) => {
